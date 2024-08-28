b008a39d25deb91c74e3e4491e7b913b3cde6067
Campbell <campbellseymour@gmail.com>
2024-08-28 13:05:08 -0400

    comp110

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,16 @@
+"""Challenge Question 1 """
+
+__author__ = 730674510
+
+
+def mimic(message: str) -> str:
+    """Returns what you put in"""
+    return message
+
+
+def main() -> None:
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first excercise in COMP110"""
+
+__author__ = "730674510"
+
+
+def greet(name: str) -> str:
+    """A Welcoming First Function Definition"""
+    return "Hello," + name + "!"
